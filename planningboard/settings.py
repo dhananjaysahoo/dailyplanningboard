ce847e2fb836853152dd91bdf8fb3ef6fc1c2eed
dhananjaysahoo <sahoo.dhananjay@gmail.com>
2021-12-26 10:58:52 +0530

    Updated Settings file

--- planningboard/settings.py
+++ planningboard/settings.py
@@ -31,18 +31,17 @@ environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-b^mo@qfm00s+m(ppsz8rabxsv@wzy!38z#$)@k8xnkh3g0j0z3'
+
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+
 DEBUG = env('DJANGO_DEBUG')
 
-#ALLOWED_HOSTS = []
+
 ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS')
 
-#application = WhiteNoise(application)
-#application = DjangoWhiteNoise(application)
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Application definition
